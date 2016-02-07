6dcdd345faa1111518b03658246ba43a3a11e0ce
Jeff Tseng <jeff.tseng@physics.ox.ac.uk>
2016-02-07 18:34:03 +0000

    move to independent comments; timezone to Sudbury

--- swap/models.py
+++ swap/models.py
@@ -29,8 +29,6 @@ class Booking(models.Model):
   approval = models.ForeignKey('Approver', related_name='approval',
                                null=True, default=None,
                                help_text="booking approved by whom")
-  user_note = models.TextField(blank=True)
-  approver_note = models.TextField(blank=True)
   begin_time = models.DateTimeField()
   end_time = models.DateTimeField()
   request_time = models.DateTimeField(auto_now_add=True)
@@ -38,4 +36,15 @@ class Booking(models.Model):
 
   def __str__(self):
     return "{0} {1} ({2})".format(self.begin_time, self.user, self.resource)
+
+class Comment(models.Model):
+  # a comment on a booking
+  commenter = models.ForeignKey('auth.User')
+  booking = models.ForeignKey('Booking', related_name='comment')
+  time = models.DateTimeField(auto_now_add=True)
+  text = models.TextField(blank=True)
+
+  def __str__(self):
+    return "{0} at {1} regarding {2}".format(
+      self.commenter, self.time, self.booking)
 
--- swap/views.py
+++ swap/views.py
@@ -11,22 +11,28 @@ class ResourceList(ListView):
 
 # bookings
 
-def bookings_by_group_charged(request, group):
-  if group != None:
-    bs = Booking.objects.filter(group=group).order_by('begin_time')
-  else:
-    bs = Booking.objects.order_by('begin_time')
-  context = { 'bookings': bs }
-  return render(request, 'swap/bookings_list.html', context)
-
+@login_required
 def bookings(request):
   # list bookings according to query
   qs = request.GET
   kwargs = {}
-  for k in [ 'user', 'charged', 'owner', 'approver', 'resource',
-             'min_begin', 'max_begin', 'min_end', 'max_end' ]:
-    if qs.get(k) != None:
-      kwargs[k] = qs.get(k)
+  for k,v in [ ('u', 'user__pk'), # user
+               ('c', 'group__pk'), # group to charge
+               ('o', 'resource__group__pk'), # resource "owner"
+               ('a', 'approval__user__pk'), # approver
+               ('r', 'resource__pk'), # resource id
+             ]:
+    n = qs.get(k)
+    if n != None and n.isnumeric():
+      kwargs[v] = n
+  for k,v in [ ('b0', 'begin_time__gte'), # minimum begin_time
+               ('b1', 'begin_time__lte'), # maximum begin_time
+               ('e0', 'end_time__gte'), # minimum end_time
+               ('e1', 'end_time__lte'), # maximum end_time
+             ]:
+    n = qs.get(k)
+    if n != None and n.isnumeric():
+      kwargs[v] = datetime.datetime.utcfromtimestamp(int(n))
   if len(kwargs) > 0:
     bs = Booking.objects.filter(kwargs).order_by('begin_time')
   else:
@@ -36,9 +42,11 @@ def bookings(request):
 
 # request/approve bookings
 
+@login_required
 def request_resource(request, resource):
   pass
 
+@login_required
 def approve_request(request, req):
   pass
 
